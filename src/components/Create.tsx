a93dd1533b69c44db63ec21910ac9066fc14a27b
Dinara Ashurova <d.ashurova@dxa.gov.az>
2024-01-19 12:41:12 +0400

    change form in modal

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,10 +1,10 @@
 // import { useState } from 'react';
 import { useState} from "preact/hooks";
-import {Button, Modal} from 'antd';
-import {SubmitHandler, useForm,} from 'react-hook-form';
+import {SubmitHandler, useForm, Controller} from 'react-hook-form';
 import {useAppDispatch} from "../hook.ts";
 import {createPost} from "../store/postSlice.ts";
 import { v4 as uuid } from 'uuid';
+import { Button, Form, Input, Modal } from 'antd';
 
 const Create:React.FC = () => {
 
@@ -17,10 +17,10 @@ const Create:React.FC = () => {
         setIsModalOpen(true);
     }
 
-    // const handleOk = () => {
-    //     setIsModalOpen(false);
-    // }
-
+ /*   const handleOk = () => {
+        setIsModalOpen(false);
+    }
+*/
     const handleCancel = () => {
         // dispatch(createPost(data))
         setIsModalOpen(false);
@@ -35,16 +35,16 @@ const Create:React.FC = () => {
     }
 
 
-   // const onFinish = (values: any) => {
-   //     console.log('Success:', values);
-   // }
-   // const onFinishFailed = (errorInfo: any) => {
-   //     console.log('Failed:', errorInfo);
-   // }
+   const onFinish = (values: any) => {
+       console.log('Success:', values);
+   }
+   const onFinishFailed = (errorInfo: any) => {
+       console.log('Failed:', errorInfo);
+   }
 
    //react-hook-form (в качестве Generic useForm принимает описание структуры нашей формы, которую мы задали - FormValues)
     //+ можем передать дополнительные параметры
-    const {register,  handleSubmit, reset} = useForm<FormValues>(
+    const {register,  control,handleSubmit, reset} = useForm<FormValues>(
         {
             defaultValues: {
                 id: uuid(),
@@ -78,57 +78,53 @@ const Create:React.FC = () => {
             <Button type="primary" onClick={showModal}>
                 Create
             </Button>
-            <Modal title="Basic Modal" open={isModalOpen} onCancel={handleCancel}>
-                <form onSubmit={handleSubmit(onSubmit)}>
+            <Modal title="Create a post" open={isModalOpen} onCancel={handleCancel}
+                   footer={[
+                       <Button type="primary">Cancel</Button>,
+                       <Button type="primary" htmlType="submit" onClick={handleSubmit(onSubmit)}>Ok</Button>
+                   ]}
+            >
+                {/*<form onSubmit={handleSubmit(onSubmit)}>*/}
 
-                    {/*<Input placeholder="Basic usage" {...register("title")}/>*/}
-                    {/*<Input placeholder="Basic usage" {...register("body")}/>*/}
-                    <input type="text" {...register("title")}/>
-                    <input type="text" {...register("body")}/>
+                {/*    /!*<Input placeholder="Basic usage" {...register("title")}/>*!/*/}
+                {/*    /!*<Input placeholder="Basic usage" {...register("body")}/>*!/*/}
+                {/*    <input type="text" {...register("title")}/>*/}
+                {/*    <input type="text" {...register("body")}/>*/}
 
-                    <input type="submit"/>
-                </form>
+                {/*    <input type="submit"/>*/}
+                {/*</form>*/}
 
-                {/*<Form*/}
-                {/*    name="basic"*/}
-                {/*    labelCol={{ span: 8 }}*/}
-                {/*    wrapperCol={{ span: 16 }}*/}
-                {/*    style={{ maxWidth: 600 }}*/}
-                {/*    initialValues={{ remember: true }}*/}
-                {/*    onFinish={onFinish}*/}
-                {/*    onFinishFailed={onFinishFailed}*/}
-                {/*    autoComplete="off"*/}
-                {/*>*/}
-                {/*    <Form.Item<FieldType>*/}
-                {/*        label="Username"*/}
-                {/*        name="username"*/}
-                {/*        rules={[{ required: true, message: 'Please input your username!' }]}*/}
-                {/*    >*/}
-                {/*        <Input />*/}
-                {/*    </Form.Item>*/}
+                <Form
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                    id="postForm"
 
-                {/*    <Form.Item<FieldType>*/}
-                {/*        label="Password"*/}
-                {/*        name="password"*/}
-                {/*        rules={[{ required: true, message: 'Please input your password!' }]}*/}
-                {/*    >*/}
-                {/*        <Input.Password />*/}
-                {/*    </Form.Item>*/}
 
-                {/*    <Form.Item<FieldType>*/}
-                {/*        name="remember"*/}
-                {/*        valuePropName="checked"*/}
-                {/*        wrapperCol={{ offset: 8, span: 16 }}*/}
-                {/*    >*/}
-                {/*        <Checkbox>Remember me</Checkbox>*/}
-                {/*    </Form.Item>*/}
+                >
+                    <Form.Item<FormValues>>
+                        <Input {...register("title")}/>
+                    </Form.Item>
 
-                {/*    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>*/}
-                {/*        <Button type="primary" htmlType="submit">*/}
-                {/*            Submit*/}
-                {/*        </Button>*/}
-                {/*    </Form.Item>*/}
-                {/*</Form>*/}
+                    {/*<Form.Item<FormValues>*/}
+
+                    {/*>*/}
+                    {/*    <Controller*/}
+                    {/*        as={Input}*/}
+                    {/*        control={control}*/}
+                    {/*        name="title"*/}
+                    {/*       defaultValue=""*/}
+                    {/*        />*/}
+                    {/*</Form.Item>*/}
+
+                    {/*<Form.Item>*/}
+                    {/*    <Button type="primary" htmlType="submit">*/}
+                    {/*        Submit*/}
+                    {/*    </Button>*/}
+                    {/*</Form.Item>*/}
+                </Form>
             </Modal>
 
         </div>
